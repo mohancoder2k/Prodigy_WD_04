84efe41d4a627998d2016af91066981149baa8b3
mohancoder2k <mohanbalu2k5@gmail.com>
2024-05-18 19:24:29 +0530

    Resume Updated

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ export default function About()
     <div className='md:w-1/2 flex justify-center'>
             <div className='flex flex-col justify-center text-white'>
                 <h1 className='text-4xl border-b-4 border-primary mb-5 w-[170px] font-bold'>About Me</h1>
-            <p className=' pb-5'>Hi, My name is Mohan, I'm a Full Stack Developer, I built beautiful websites using resct js and tailwind css.</p>
+            <p className=' pb-5'>Hi, My name is Mohan, I'm a Full Stack Developer, I built beautiful websites using react js and tailwind css.</p>
             <p className='pb-5'>I am proficient in Frontend skills like HTML, CSS and Framework React JS.</p>
             <p className=''>In backend I know Spring Boot, Node JS</p>
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 export default function Contact(){
-    return <section id='contact' className='flex flex-col bg-primary px-5 py-32 text-white'>
-         <div className='flex flex-col items-center'>
+    return <section id='contact' className='flex flex-col bg-primary px-5 py-32 text-white h-auto md:h-[600px]'>
+         <div className='flex flex-col items-center '>
            
            <h1 className='text-4xl border-b-4  border-secondary mb-5 w-[140px] font-bold'>Contact</h1>
            <p className='pb-5'>If you want to discuss more in detail, please contact me</p>
